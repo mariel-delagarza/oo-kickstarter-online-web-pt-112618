f75c1fd111eb35f8aa5c7215459ef2fc0e7c987f
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-01-21 17:42:27 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,14 @@
+class Project
+  attr_reader :name
+  attr_accessor :backers 
+
+  def initialize(name) 
+    @name = name 
+    @backers = []
+    if (self.backer.nil?)
+      self.backer = Backer.new(name)
+    else 
+      self.backer.name = name 
+  end 
+  
+end 
